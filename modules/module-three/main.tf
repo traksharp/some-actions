b7476c9961053010ff2c921a891df46536f6ddef
André Dumas <agdumas@gmail.com>
2024-06-14 20:06:48 +1000

    feat: Add new module

---
+++ modules/module-three/main.tf
@@ -0,0 +1,5 @@
+locals {
+  version = "v3.0.1"  # x-release-please-version
+  short_version = "3.0.1"  # x-release-please-version
+  module_version = "module-one-v3.0.1"  # x-release-please-version
+}
